acaa53f84a4004c3c3820d2bc8b7cb1e21268535
fummicc1 <fumiya.tennis1@gmail.com>
2020-05-10 00:51:33 +0900

    wip [add]: same symbols.

--- Sources/MNCalendar/MNCalendar.swift
+++ Sources/MNCalendar/MNCalendar.swift
@@ -1,3 +1,86 @@
-struct MNCalendar {
-    var text = "Hello, World!"
+import Foundation
+
+public protocol MNCalendarType {
+    func getNumberOfDaysInMonth(from date: Date) -> Int
+    func getNumberOfDaysInWeek(from date: Date) -> Int
+    func getMonth(of date: Date) -> Int
+    func getYear(of date: Date) -> Int
+    func moveToNextMonth()
+    func moveToPreviousMonth()
+    func getNumberOfItemsForCurrentMode() -> (dates: [Date], count: Int)
+}
+
+public enum MNCalendarMode {
+    case week
+    case month
+}
+
+public class MNCalendar {
+    private let calendar: Calendar
+    private let timeZone: TimeZone
+
+    private(set) var mode: MNCalendarMode = .month
+    private var currentDates: [Date] = []
+    private var currentDate: Date {
+        didSet {
+            updateCurrent(withDate: currentDate)
+        }
+    }
+    private var currentComponents: DateComponents {
+        calendar.dateComponents(in: timeZone, from: currentDate)
+    }
+    
+    public init(calendar: Calendar = .current, timeZone: TimeZone = .current, initialDate: Date = .init()) {
+        self.calendar = calendar
+        self.timeZone = timeZone
+        self.currentDate = initialDate
+    }
+    
+    private func updateCurrent(withDate date: Date) {
+        
+    }
+}
+
+extension MNCalendar: MNCalendarType {
+    public func getNumberOfDaysInMonth(from date: Date) -> Int {
+        
+    }
+    
+    public func getNumberOfDaysInWeek(from date: Date) -> Int {
+        
+    }
+    
+    public func getMonth(of date: Date) -> Int {
+        calendar.component(.month, from: date)
+    }
+    
+    public func getYear(of date: Date) -> Int {
+        calendar.component(.year, from: date)
+    }
+    
+    public func moveToNextMonth() {
+        var components = DateComponents()
+        components.day = 1
+        guard let updatedDate = calendar.date(byAdding: components, to: currentDate) else {
+            assert(false)
+            return
+        }
+        self.currentDate = updatedDate
+    }
+    
+    public func moveToPreviousMonth() {
+        var components = DateComponents()
+        components.day = -1
+        guard let updatedDate = calendar.date(byAdding: components, to: currentDate) else {
+            assert(false)
+            return
+        }
+        self.currentDate = updatedDate
+    }
+    
+    public func getNumberOfItemsForCurrentMode() -> (dates: [Date], count: Int) {
+        fatalError()
+    }
+    
+    
 }
